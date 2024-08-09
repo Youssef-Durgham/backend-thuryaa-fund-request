06fc0409b10c34672d1da0c13cee3dec6853f7e9
Youssef-Durgham <yusifdhrgamtrt@gmail.com>
2024-08-10 00:37:20 +0300

    done

--- routes/CustomerUsersList.js
+++ routes/CustomerUsersList.js
@@ -2,8 +2,43 @@ const express = require('express');
 const { Admin, Customer } = require('../model/Users'); // Adjust the path as needed
 const jwt = require('jsonwebtoken');
 const ActivityLog = require('../model/ActivityLog');
+const { Role } = require('../model/Role');
 
 const router = express.Router();
+
+const checkPermission = (permission) => {
+  return async (req, res, next) => {
+    console.log(req.headers.authorization, "by func");
+    try {
+      const token = req.headers.authorization.split(' ')[1];
+      console.log(token);
+      
+      const decoded = jwt.verify(token, 'your_jwt_secret');
+      console.log(decoded);
+      console.log(permission, token, decoded);
+
+      const admin = await Admin.findById(decoded.id).populate('roles');
+
+      // Check permissions in directly assigned roles
+      const hasPermission = admin.roles.some(role =>
+        role.permissions.includes(permission)
+      );
+
+      console.log(permission, token, decoded, admin, hasPermission);
+
+      if (!hasPermission) {
+        return res.status(403).json({ message: 'Forbidden' });
+      }
+
+      req.adminId = decoded.id; // Store the admin ID in the request object
+      next();
+    } catch (error) {
+      console.log("JWT Verification Error:", error.message);
+      console.log(error.stack);
+      res.status(401).json({ message: 'Unauthorized', error: error.message });
+    }
+  };
+};
 
 // Get customer User List
 router.get('/customers-list', async (req, res) => {
@@ -86,6 +121,28 @@ router.get('/login-history/:customerId', async (req, res) => {
       } catch (error) {
       res.status(500).json({ message: 'Server error', error });
       }
+});
+
+// Search customers by phone or name
+router.get('/search-customers', checkPermission('create_order'), async (req, res) => {
+  const { query } = req.query;
+
+  if (!query) {
+    return res.status(400).json({ error: 'Query parameter is required' });
+  }
+
+  try {
+    const customers = await Customer.find({
+      $or: [
+        { phone: { $regex: query, $options: 'i' } },
+        { name: { $regex: query, $options: 'i' } }
+      ]
+    }).select('_id phone name'); // Include _id along with phone and name
+
+    res.json(customers);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while searching for customers' });
+  }
 });
   
   module.exports = router;
